5519bdb73ceea18c941a1f79759a2116d3efd7b0
David Deng <daviddengcn@gmail.com>
2013-12-08 16:45:31 -0800

    ADD Sophie interface, type Int32/String/ByteArray/VInt

---
+++ io.go
@@ -0,0 +1,126 @@
+package sophie
+
+import (
+	"fmt"
+	"io"
+)
+
+type Reader interface {
+	io.Reader
+	io.ByteReader
+}
+
+type Writer interface {
+	io.Writer
+	io.ByteWriter
+}
+
+type Sophier interface {
+	ReadFrom(r Reader) error
+	WriteTo(w Writer) error
+}
+
+// *SInt32 implements Sophie interface
+type Int32 int32
+
+func (i Int32) WriteTo(w Writer) error {
+	arr := [4]byte{byte(i), byte(i >> 8), byte(i >> 16), byte(i >> 24)}
+	_, err := w.Write(arr[:])
+	return err
+}
+
+func (i *Int32) ReadFrom(r Reader) error {
+	var arr [4]byte
+	n, err := r.Read(arr[:])
+	if n < 4 {
+		return err
+	}
+
+	*i = Int32(arr[0]) | Int32(arr[1])<<8 | Int32(arr[2])<<16 |
+		Int32(arr[3])<<24
+	return nil
+}
+
+// *SVInt implements Sophie interface and serializing as a vint
+type VInt uint64
+
+func (i VInt) WriteTo(w Writer) error {
+	var arr [10]byte
+	n := 0
+	for i > 0x7f {
+		arr[n] = byte(i&0x7f) | 0x80
+		n++
+		i >>= 7
+	}
+	arr[n] = byte(i)
+	n++
+	_, err := w.Write(arr[:n])
+	return err
+}
+
+func (i *VInt) ReadFrom(r Reader) error {
+	var v VInt
+	b, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+	v = VInt(b & 0x7f)
+	for n := VInt(7); b & 0x80 != 0; n += 7 {
+		b, err = r.ReadByte()
+		if err != nil {
+			return err
+		}
+		v |= VInt(b & 0x7f) << n
+	}
+	*i = v
+	return nil
+}
+
+func (i *VInt) String() string {
+	return fmt.Sprint(*i)
+}
+
+type ByteArray []byte
+
+func (ba ByteArray) WriteTo(w Writer) error {
+	if err := VInt(len(ba)).WriteTo(w); err != nil {
+		return err
+	}
+	_, err := w.Write(ba)
+	return err
+}
+
+func (ba *ByteArray) ReadFrom(r Reader) error {
+	var l VInt
+	if err := l.ReadFrom(r); err != nil {
+		return err
+	}
+	if VInt(cap(*ba)) < l {
+		*ba = make(ByteArray, l)
+	}
+	
+	if VInt(len(*ba)) != l {
+		*ba = (*ba)[:l]
+	}
+	
+	_, err := r.Read(*ba)
+	return err
+}
+
+// *SString implements Sophie interface
+type String string
+
+func (s String) WriteTo(w Writer) error {
+	return ByteArray(s).WriteTo(w)
+}
+
+func (s *String) ReadFrom(r Reader) error {
+	var ba ByteArray
+	if err := ba.ReadFrom(r); err != nil {
+		return err
+	}
+	
+	*s = String(ba)
+	
+	return nil
+}
---
+++ io_test.go
@@ -0,0 +1,93 @@
+package sophie
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/daviddengcn/go-assert"
+)
+
+func readWrite(t *testing.T, sa, sb Sophier, outBytes int) {
+	var buf bytes.Buffer
+	assert.NoErrorf(t, fmt.Sprintf("readWrite(%v): sa.WriteTo failed: %%v", sa),
+		sa.WriteTo(&buf))
+
+	if outBytes >= 0 {
+		assert.Equals(t, fmt.Sprintf("readWrite(%v): buf.Len", sa), buf.Len(),
+			outBytes)
+	}
+
+	assert.NoErrorf(t, fmt.Sprintf("readWrite(%v): sb.ReadFrom failed: %%v",
+		sa), sb.ReadFrom(&buf))
+}
+
+func TestBasicSophieTypes(t *testing.T) {
+	// Test of nt32
+	var i32a, i32b Int32
+	i32a = 1234
+	readWrite(t, &i32a, &i32b, 4)
+	assert.Equals(t, "i32b", i32b, i32a)
+
+	i32a = -1234
+	readWrite(t, &i32a, &i32b, 4)
+	assert.Equals(t, "i32b", i32b, i32a)
+	// Test of VInt
+	var via, vib VInt
+	via = 0
+	readWrite(t, &via, &vib, 1)
+	assert.Equals(t, "vib", vib, via)
+	via = 127
+	readWrite(t, &via, &vib, 1)
+	assert.Equals(t, "vib", vib, via)
+	via = 128
+	readWrite(t, &via, &vib, 2)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X3FFE
+	readWrite(t, &via, &vib, 2)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X4002
+	readWrite(t, &via, &vib, 3)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X1FF2FF
+	readWrite(t, &via, &vib, 3)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X20FF01
+	readWrite(t, &via, &vib, 4)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X0FFFFF01
+	readWrite(t, &via, &vib, 4)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X10000005
+	readWrite(t, &via, &vib, 5)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X7FFFFFF01
+	readWrite(t, &via, &vib, 5)
+	assert.Equals(t, "vib", vib, via)
+	via = 0X800000005
+	readWrite(t, &via, &vib, 6)
+	assert.Equals(t, "vib", vib, via)
+	// Test of String, and ByteArray
+	var sa, sb String
+	sa = ""
+	readWrite(t, &sa, &sb, 1)
+	assert.Equals(t, "sb", sb, sa)
+	
+	sa = "Hello"
+	readWrite(t, &sa, &sb, 6)
+	assert.Equals(t, "sb", sb, sa)
+	
+	sa = ""
+	for len(sa) < 127 {
+		sa += "a"
+	}
+	readWrite(t, &sa, &sb, 128)
+	assert.Equals(t, "sb", sb, sa)
+	sa = ""
+	
+	for len(sa) < 128 {
+		sa += "a"
+	}
+	readWrite(t, &sa, &sb, 130)
+	assert.Equals(t, "sb", sb, sa)
+}
